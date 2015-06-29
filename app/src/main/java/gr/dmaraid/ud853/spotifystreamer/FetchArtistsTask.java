37e0500e58cd18cae6f313c43750bef8a572e098
Dimitris Maraidonis <dmaraid@gmail.com>
2015-06-29 15:13:11 +0300

    Added internet connectivity check.

--- app/src/main/java/gr/dmaraid/ud853/spotifystreamer/FetchArtistsTask.java
+++ app/src/main/java/gr/dmaraid/ud853/spotifystreamer/FetchArtistsTask.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.List;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
@@ -17,7 +18,8 @@ public class FetchArtistsTask extends AsyncTask<String, Void, My_Artist[]> {
 
     private Context context;
     //private String TAG = FetchArtistsTask.class.getSimpleName();
-    private int searchSize=0;
+    private int searchSize = 0;
+    boolean hasInternetAccess = false;
 
     public FetchArtistsTask(Context context) {
         this.context = context;
@@ -26,51 +28,77 @@ public class FetchArtistsTask extends AsyncTask<String, Void, My_Artist[]> {
     @Override
     protected My_Artist[] doInBackground(String... given_artist) {
 
-        SpotifyApi api = new SpotifyApi();
-        SpotifyService spotify = api.getService();
+        if (isOnline()) {
+            hasInternetAccess = true;
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
 
-        ArtistsPager searchArtists = spotify.searchArtists(given_artist[0]);
+            ArtistsPager searchArtists = spotify.searchArtists(given_artist[0]);
 
-        My_Artist my_artists[] = null;
+            My_Artist my_artists[] = null;
 
-        searchSize = searchArtists.artists.total;
-        if (searchSize > 0) {
-            if (searchSize > searchArtists.artists.limit) {
-                my_artists = new My_Artist[searchArtists.artists.limit];
-            } else {
-                my_artists = new My_Artist[searchSize];
-            }
-
-            List<Artist> listOfArtists = searchArtists.artists.items;
-
-            String artist_name;
-            String artist_id;
-            for (int i = 0; i < listOfArtists.size(); i++) {
-
-                artist_name = listOfArtists.get(i).name;
-                artist_id = listOfArtists.get(i).id;
-                List<Image> listOfImages = listOfArtists.get(i).images;
-                String artist_image = "file:///android_asset/no_image_available.jpg";
-                if (listOfImages.size() > 0) {
-                    for (int j = 0; j < listOfImages.size(); j++) {
-                        if (listOfImages.get(j).height <= 300 && listOfImages.get(j).height >= 100) {
-                            artist_image = listOfImages.get(j).url;
-                        }
-                    }
+            searchSize = searchArtists.artists.total;
+            if (searchSize > 0) {
+                if (searchSize > searchArtists.artists.limit) {
+                    my_artists = new My_Artist[searchArtists.artists.limit];
+                } else {
+                    my_artists = new My_Artist[searchSize];
                 }
 
-                my_artists[i] = new My_Artist(artist_image, artist_name, artist_id);
-                //Log.i(TAG, "Name: " + my_artists[i].getName() + ", Image URL: " + my_artists[i].getIcon());
-            }
-        }
+                List<Artist> listOfArtists = searchArtists.artists.items;
 
-        return my_artists;
+                String artist_name;
+                String artist_id;
+                for (int i = 0; i < listOfArtists.size(); i++) {
+
+                    artist_name = listOfArtists.get(i).name;
+                    artist_id = listOfArtists.get(i).id;
+                    List<Image> listOfImages = listOfArtists.get(i).images;
+                    String artist_image = "file:///android_asset/no_image_available.jpg";
+                    if (listOfImages.size() > 0) {
+                        for (int j = 0; j < listOfImages.size(); j++) {
+                            if (listOfImages.get(j).height <= 300 && listOfImages.get(j).height >= 100) {
+                                artist_image = listOfImages.get(j).url;
+                            }
+                        }
+                    }
+
+                    my_artists[i] = new My_Artist(artist_image, artist_name, artist_id);
+                    //Log.i(TAG, "Name: " + my_artists[i].getName() + ", Image URL: " + my_artists[i].getIcon());
+                }
+            }
+
+            return my_artists;
+        } else {
+            return null;
+        }
     }
 
     @Override
     protected void onPostExecute(My_Artist[] my_artists) {
-        if (searchSize == 0) {
-            Toast.makeText(context, context.getString(R.string.empty_search), Toast.LENGTH_LONG).show();
+        if (!hasInternetAccess) {
+            Toast.makeText(context, context.getString(R.string.no_internet_access), Toast.LENGTH_LONG).show();
+        } else {
+            if (searchSize == 0) {
+                Toast.makeText(context, context.getString(R.string.empty_search), Toast.LENGTH_LONG).show();
+            }
         }
     }
+
+    boolean isOnline() {
+
+        Runtime runtime = Runtime.getRuntime();
+        try {
+
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
 }
--- app/src/main/java/gr/dmaraid/ud853/spotifystreamer/FetchTop10Task.java
+++ app/src/main/java/gr/dmaraid/ud853/spotifystreamer/FetchTop10Task.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +20,7 @@ public class FetchTop10Task extends AsyncTask<String, Void, Top10[]> {
 
     private Context context;
     boolean hasTop10 = false;
+    boolean hasInternetAccess = false;
 
     public FetchTop10Task(Context context) {
         this.context = context;
@@ -27,47 +29,73 @@ public class FetchTop10Task extends AsyncTask<String, Void, Top10[]> {
     @Override
     protected Top10[] doInBackground(String... chosen_artist) {
 
-        SpotifyApi api = new SpotifyApi();
-        SpotifyService spotify = api.getService();
+        if (isOnline()) {
+            hasInternetAccess = true;
 
-        Map<String, Object> map = new HashMap<>();
-        map.put("country", "GR");
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
 
-        Tracks searchArtists = spotify.getArtistTopTrack(chosen_artist[0], map);
-        Top10 top10[] = new Top10[0];
+            Map<String, Object> map = new HashMap<>();
+            map.put("country", "GR");
 
-        List<Track> tracks = searchArtists.tracks;
-        if (tracks.size() != 0) {
-            hasTop10 = true;
-            if (tracks.size() >= 10) {
-                top10 = new Top10[10];
-            } else {
-                top10 = new Top10[tracks.size()];
-            }
-            for (int i = 0; i < tracks.size(); i++) {
-                String song = tracks.get(i).name;
-                String album = tracks.get(i).album.name;
-                String top10_image = "file:///android_asset/no_image_available.jpg";
+            Tracks searchArtists = spotify.getArtistTopTrack(chosen_artist[0], map);
+            Top10 top10[] = new Top10[0];
 
-                List<Image> listOfImages = tracks.get(i).album.images;
-                if (listOfImages.size() > 0) {
-                    for (int j = 0; j < listOfImages.size(); j++) {
-                        if (listOfImages.get(j).height <= 300 && listOfImages.get(j).height >= 100) {
-                            top10_image = listOfImages.get(j).url;
+            List<Track> tracks = searchArtists.tracks;
+            if (tracks.size() != 0) {
+                hasTop10 = true;
+                if (tracks.size() >= 10) {
+                    top10 = new Top10[10];
+                } else {
+                    top10 = new Top10[tracks.size()];
+                }
+                for (int i = 0; i < tracks.size(); i++) {
+                    String song = tracks.get(i).name;
+                    String album = tracks.get(i).album.name;
+                    String top10_image = "file:///android_asset/no_image_available.jpg";
+
+                    List<Image> listOfImages = tracks.get(i).album.images;
+                    if (listOfImages.size() > 0) {
+                        for (int j = 0; j < listOfImages.size(); j++) {
+                            if (listOfImages.get(j).height <= 300 && listOfImages.get(j).height >= 100) {
+                                top10_image = listOfImages.get(j).url;
+                            }
                         }
                     }
+                    top10[i] = new Top10(top10_image, song, album);
                 }
-                top10[i] = new Top10(top10_image, song, album);
             }
+            return top10;
+        } else {
+            return null;
         }
-        return top10;
     }
 
     @Override
     protected void onPostExecute(Top10[] top10s) {
         super.onPostExecute(top10s);
-        if (!hasTop10) {
-            Toast.makeText(context, context.getString(R.string.empty_top10), Toast.LENGTH_LONG).show();
+        if (!hasInternetAccess) {
+            Toast.makeText(context, context.getString(R.string.no_internet_access), Toast.LENGTH_LONG).show();
+        } else {
+            if (!hasTop10) {
+                Toast.makeText(context, context.getString(R.string.empty_top10), Toast.LENGTH_LONG).show();
+            }
         }
     }
+
+    boolean isOnline() {
+
+        Runtime runtime = Runtime.getRuntime();
+        try {
+
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+
+        } catch (IOException e)          { e.printStackTrace(); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+
+        return false;
+    }
+
 }
